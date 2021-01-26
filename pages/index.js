350adcdc14f94639479ffa71cbeaebf6b04e66cb
Guilherme Takimoto <guilherme.takimoto@zatos.com.br>
2021-01-26 00:53:12 +0000

    Código ao final da primeira aula da Imersão React

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,56 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import QuizBackground from '../src/components/QuizBackground'
+import QuizLogo from '../src/components/QuizLogo'
+import Widget from '../src/components/Widget'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+
+          <Widget.Content>
+            lorem ipsum
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            Contribua para aquele suco:
+            <br /><br />
+            <form action="https://www.paypal.com/donate" method="post" target="_top">
+              <input type="hidden" name="business" value="guilhermetakimoto@hotmail.com" />
+              <input type="hidden" name="item_name" value="Beer!" />
+              <input type="hidden" name="currency_code" value="BRL" />
+              <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+              <img alt="" border="0" src="https://www.paypal.com/en_BR/i/scr/pixel.gif" width="1" height="1" />
+            </form>
+
+          </Widget.Content>
+        </Widget>
+        
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl={db.gitHubUrl} />
+    </QuizBackground>
+  )
 }
