612b86440e9bab325f6c5c29ad1e9e07f8f7d2e9
Guilherme Takimoto <guilherme.takimoto@zatos.com.br>
2021-02-01 01:44:37 +0000

    Ajuste de CSS de componente

--- src/components/QuizContainer/index.js
+++ src/components/QuizContainer/index.js
@@ -1,14 +1,10 @@
 import styled from 'styled-components'
 
 const QuizContainer = styled.div`
-  width: 30%;
+  width: 100%;
   max-width: 350px;
   padding-top: 45px;
   margin: auto 10%;
-  /* margin-left: 10%;
-  margin-top: auto;
-  margin-bottom: auto;
-  margin-right: 0px; */
   @media screen and (max-width: 500px) {
     margin: auto;
     padding: 15px;
